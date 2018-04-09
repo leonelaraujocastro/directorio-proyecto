42a284bc01d3b191041b026d129d580d2bd5450f
Leonel Araujo <araujo_leonel@hotmail.com>
2018-04-09 09:56:11 -0430

    Primera version del proyecto

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Página de Pruebas</title>
+        <link href="css/bootstrap4.min.css" rel="stylesheet" type="text/css">
+        <script src="js/jquery-3.2.1.min.js"></script>
+        <script src="js/bootstrap4.min.js"></script>
+        <script type = "text/javascript">
+            $(document).ready(function(){
+		  
+            });		  
+        </script>	
+    </head>
+    <body>
+	    <h3>Esta es una Prueba</h3>
+    </body>
+</html>
